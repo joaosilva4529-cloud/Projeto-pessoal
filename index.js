2a0a9624c81c0bdc88a53804ed342188718c7cdb
João Vitor Cruz <joao.silva4529@aluno.ce.gov.br>
2026-09-24 11:54:07 -0300

    AGR SIM EM

---
+++ index.js
@@ -0,0 +1,35 @@
+//Informaçõees do cronograma
+const dia="Segunda-feira"
+
+
+//Verificação de data//
+if (dia==="Segunda-feira") {
+    console.log("1°Estude Portugues e Redação. Reveja anotações sobre figuras de palavras com atenção aos tipos de substantivos. 2° Entenda a estrutura básica de uma texto argumentativo e funçoes de cada fase de um. ")
+}
+
+else if (dia==="Terça-feira") {
+    console.log("Estude Html/css e Logica de progamação. Revise estrutura básica de um site funções de cada tag e como encaixar cada componente. 2° Estude a estrutura básica de um código js e pratique com resolução de problemas")
+}
+
+else if (dia==="Quarta-feira") {
+    console.log("Estude Química e Biologia.  1° Revise sobre distribuição eletrónica,tabela periódica,propiedades de um átomo e ligações químicas. 2° Dé uma olhada em tipos de reprodução (sexuada e asexuada) e suas diferenças")
+}
+
+else if (dia==="Quinta-feira") {
+    console.log("Estude Portuguez e Redação.")
+}
+
+else if (dia==="Sexta-feira") {
+    console.log("Estude Química e Biologia.")
+}
+
+else if (dia==="Sábado-feira") {
+    console.log("Estude Matemática e Física.")
+}
+
+else if (dia==="Domingo-feira") {
+    console.log("Estude História e Geografia.")
+}
+
+
+
