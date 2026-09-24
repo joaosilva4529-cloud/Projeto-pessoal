d07d8e537c4d736275b670fb1e2ebc6c0480be36
João Vitor Cruz <joao.silva4529@aluno.ce.gov.br>
2026-09-24 13:10:47 -0300

    AGR SIM2.0

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 //Informaçõees do cronograma
-const dia="Segunda-feira"
+const dia="Terça-feira"
 
 
-//Verificação de data//
-if (dia==="Segunda-feira") {
+//Verificação de data + oque eu estudo hoje?//
+if (dia === "Segunda-feira") {
     console.log("1°Estude Portugues e Redação. Reveja anotações sobre figuras de palavras com atenção aos tipos de substantivos. 2° Entenda a estrutura básica de uma texto argumentativo e funçoes de cada fase de um. ")
 }
 
-else if (dia==="Terça-feira") {
-    console.log("Estude Html/css e Logica de progamação. Revise estrutura básica de um site funções de cada tag e como encaixar cada componente. 2° Estude a estrutura básica de um código js e pratique com resolução de problemas")
+else if (dia === "Terça-feira") {
+    console.log("Conteúdo de hoje -> Html/css e Logica de progamação. Revise estrutura básica de um site funções de cada tag e como encaixar cada componente. 2° Estude a estrutura básica de um código js e pratique com resolução de problemas")
 }
 
-else if (dia==="Quarta-feira") {
-    console.log("Estude Química e Biologia.  1° Revise sobre distribuição eletrónica,tabela periódica,propiedades de um átomo e ligações químicas. 2° Dé uma olhada em tipos de reprodução (sexuada e asexuada) e suas diferenças")
+else if (dia === "Quarta-feira") {
+    console.log("Conteúdo de hoje -> Química e Biologia.  1° Revise sobre distribuição eletrónica,tabela periódica,propiedades de um átomo e ligações químicas. 2° Dé uma olhada em tipos de reprodução (sexuada e asexuada) e suas diferenças")
 }
 
-else if (dia==="Quinta-feira") {
-    console.log("Estude Portuguez e Redação.")
+else if (dia === "Quinta-feira") {
+    console.log("Conteúdo de hoje -> História e Geografia. 1° História Egípcia, contendo economia,estrutura,hierarquia,deuses,cultura e tudo mais da. 2° Compreenda fatores do clima,climogramas,tipos de ventos e como eles se comportam. ")
 }
 
-else if (dia==="Sexta-feira") {
-    console.log("Estude Química e Biologia.")
+else if (dia === "Sexta-feira") {
+    console.log("Conteúdo de hoje -> Linguagems. 1° reveja falsos cognátos como ver e dizer as horas regras básicas...2° Veja verbo to be....acabou  ")
 }
 
-else if (dia==="Sábado-feira") {
-    console.log("Estude Matemática e Física.")
+else if (dia === "Sábado") {
+    console.log("Conteúdo de hoje -> Matemática e Física. 1° Estude função quadrática em geral e revise anotações sobre. 2° Leis de newton,tipos de força e revize tds ")
 }
 
-else if (dia==="Domingo-feira") {
-    console.log("Estude História e Geografia.")
+else if (dia === "Domingo") {
+    console.log("Discansse ou faça uma Revisão leve das anotações")
 }
+//caso eu digite errado//
+else {
+    console.log("Digite um dia válido")
+}
+
+
+
+
 
 
 
